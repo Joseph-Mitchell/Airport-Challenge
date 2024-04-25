c3b683901de5f55d5a56feaade2fd9a9a72e5f83
Joseph Mitchell <joejmitchell@hotmail.co.uk>
2024-04-25 16:08:44 +0100

    Updated demonstration code to include new user stories

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import Airport from "./airport.js"
+import Airport from "./airport.js";
 
 let airport = new Airport();
 let plane;
@@ -22,8 +22,7 @@ console.log("\nCannot add to an airport at full capacity");
 console.log("==============================");
 
 airport.planes = [];
-for (let i = 0; i < 10; i++)
-    airport.assignPlane({ id: i });
+for (let i = 0; i < 10; i++) airport.assignPlane({ id: i });
 plane = { id: 10 };
 
 console.log("Before attempting: " + JSON.stringify(airport.planes));
@@ -86,3 +85,36 @@ console.log("Capacity before: " + airport.capacity);
 airport.overrideCapacity(15);
 
 console.log("Capacity after: " + airport.capacity);
+
+//User Story 7 ===========================================================================
+console.log("\nCannot add plane while weather is stormy");
+console.log("==============================");
+
+airport = new Airport();
+airport.weather = "stormy";
+plane = { id: 0 };
+
+console.log("Weather: " + airport.weather);
+console.log("Before attempting: " + JSON.stringify(airport.planes));
+console.log("Plane to add: " + JSON.stringify(plane));
+
+airport.assignPlane(plane);
+
+console.log("After attempting: " + JSON.stringify(airport.planes));
+
+//User Story 8 ===========================================================================
+console.log("\nCannot remove plane while weather is stormy");
+console.log("==============================");
+
+airport = new Airport();
+airport.weather = "stormy";
+airport.planes = [{ id: 0 }];
+plane = { id: 0 };
+
+console.log("Weather: " + airport.weather);
+console.log("Before attempting: " + JSON.stringify(airport.planes));
+console.log("Plane to remove: " + JSON.stringify(plane));
+
+airport.removePlane(plane.id);
+
+console.log("After attempting: " + JSON.stringify(airport.planes));
